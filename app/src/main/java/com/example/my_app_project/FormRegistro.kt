36aca227e41a946f872a768ecf9ab4df408fcdba
Erick Reyes <reyespoloerick1@gmail.com>
2025-04-18 17:17:27 -0500

    Pantalla principal, formRegistrar, formInicioSesion terminados

--- app/src/main/java/com/example/my_app_project/FormRegistro.kt
+++ app/src/main/java/com/example/my_app_project/FormRegistro.kt
@@ -1,35 +1,20 @@
 package com.example.my_app_project
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class FormRegistro : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_form_registro)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-        //Boton Registrar
-        val btnRegistrar = findViewById<Button>(R.id.btnRegistrar)
-        btnRegistrar.setOnClickListener{
-            val intento = Intent(this, FormRegistro::class.java)
-            startActivity(intento)
-        }
-
-        //Boton Iniciar Sesion
-        val btnIniciarSesion = findViewById<Button>(R.id.btnIniciarSesion)
-        btnIniciarSesion.setOnClickListener{
-            val intent = Intent(this,FormInicioSesion::class.java)
-            startActivity(intent)
         }
     }
 }
