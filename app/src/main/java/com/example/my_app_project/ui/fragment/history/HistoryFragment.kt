a5cdd413a8a41a293a04c80a04ba4e1fa2f0084c
SGChávezJuárez <129635350+Sergardo12@users.noreply.github.com>
2025-05-18 12:22:43 -0500

    Comentado todo lo que tenga que ser con el ServicioAdapter de Diego

--- app/src/main/java/com/example/my_app_project/ui/fragment/history/HistoryFragment.kt
+++ app/src/main/java/com/example/my_app_project/ui/fragment/history/HistoryFragment.kt
@@ -7,31 +7,31 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.my_app_project.domain.model.Servicio
-import com.example.my_app_project.ui.fragment.adapter.ServicioAdapter
+//import com.example.my_app_project.ui.fragment.adapter.ServicioAdapter
 import com.example.my_app_project.databinding.FragmentHistoryBinding
 
 class HistoryFragment : Fragment() {
 
-    private lateinit var binding: FragmentHistoryBinding
-    private lateinit var servicioAdapter: ServicioAdapter
-    private lateinit var listaHistorial: MutableList<Servicio>
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        binding = FragmentHistoryBinding.inflate(inflater, container, false)
-
-        listaHistorial = mutableListOf(
-            Servicio("Gasfitero", "Luis R.", "06/04/2025"),
-            Servicio("Electricista", "Juan T.", "03/04/2025"),
-            Servicio("Carpintero", "Pedro Z.", "01/04/2025")
-        )
-
-        servicioAdapter = ServicioAdapter(listaHistorial)
-        binding.recyclerHistorial.layoutManager = LinearLayoutManager(requireContext())
-        binding.recyclerHistorial.adapter = servicioAdapter
-
-        return binding.root
-    }
+//    private lateinit var binding: FragmentHistoryBinding
+//    private lateinit var servicioAdapter: ServicioAdapter
+//    private lateinit var listaHistorial: MutableList<Servicio>
+//
+//    override fun onCreateView(
+//        inflater: LayoutInflater, container: ViewGroup?,
+//        savedInstanceState: Bundle?
+//    ): View {
+//        binding = FragmentHistoryBinding.inflate(inflater, container, false)
+//
+//        listaHistorial = mutableListOf(
+//            Servicio("Gasfitero", "Luis R.", "06/04/2025"),
+//            Servicio("Electricista", "Juan T.", "03/04/2025"),
+//            Servicio("Carpintero", "Pedro Z.", "01/04/2025")
+//        )
+//
+//        servicioAdapter = ServicioAdapter(listaHistorial)
+//        binding.recyclerHistorial.layoutManager = LinearLayoutManager(requireContext())
+//        binding.recyclerHistorial.adapter = servicioAdapter
+//
+//        return binding.root
+    //}
 }
