162f412be27a74c37ab4290ede1949288835fea3
SGChávezJuárez <129635350+Sergardo12@users.noreply.github.com>
2025-05-18 12:13:42 -0500

    Eliminacion del servicio adapter

--- app/src/main/java/com/example/my_app_project/domain/model/Servicio.kt
+++ app/src/main/java/com/example/my_app_project/domain/model/Servicio.kt
@@ -1,14 +1,14 @@
 package com.example.my_app_project.domain.model
 
 data class Servicio(
-<<<<<<< HEAD
     val nombreServicio: String = "",
     val imagenServicio: String = ""
 ) {
+
 }
-=======
-    val nombreTrabajador: String,
-    val especialidad: String,
-    val fecha: String
-)
->>>>>>> a101b9661b711522bdea923f6f31b76ccb1d1346
+
+
+
+
+
+
